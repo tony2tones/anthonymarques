64f4d28d677876650ccfe5acb926dfa282f5e2e9
tony2tones <anthonymarques@outlook.com>
2023-07-01 11:11:56 +0200

    fixed base styles and removed margin and padding and what not
    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,17 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- 
+  public showToggle = false;
+  public hideLanding = false;
+  constructor(private router: Router){}
+
+  hasRoute(route:string) {
+    return this.router.url.includes(route);
+  }
+
+  uiUpdate($event:boolean) {
+    this.hideLanding = $event;
+    this.router.navigateByUrl('/profile');
+  }
 }
 
