d1c5e61dbfc5139be22ad1c52ef5a8c5549fe83a
tony2tones <anthonymarques@outlook.com>
2023-07-02 19:15:26 +0200

    Added routing from Landing page to main
    added basic styles and main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/pages/landing-page/landing-page.component';
+import { MainComponent } from './components/pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [{  path: 'home', component: LandingPageComponent },
+{ path: '', redirectTo: '/home' , pathMatch:'full'},
+{  path: 'main', component: MainComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/pages/main/main.module.ts
@@ -0,0 +1,11 @@
+// import { NgModule } from "@angular/core";
+// import { RouterModule } from "@angular/router";
+
+// const routes = [{}];
+
+// @NgModule({
+//     declarations: [],
+//     imports: [
+//         RouterModule.forRoot(routes),
+//       ],
+//     })
