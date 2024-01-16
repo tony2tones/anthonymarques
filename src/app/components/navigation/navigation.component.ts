255c128194ede5bf6f306b862a4c1dda4313912a
tony <anthonymarques@outlook.com>
2024-01-16 20:58:40 +0200

    working ion dismissing the menu thingy

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,11 +8,20 @@ import { Router } from '@angular/router';
 })
 export class NavHeaderComponent {
   @Output() messageBus = new EventEmitter<string>();
+  @ViewChild('navbar') navbarRef: ElementRef | undefined;
+
   constructor(private router: Router) { }
 
   goDown(sectionId:string) {
     console.log('is From the header ',sectionId)
     this.messageBus.emit(sectionId);
+    this.closeMobileMenu();
   }
+
+  closeMobileMenu() {
+    const navbar = document.getElementById('navbar') as HTMLElement;
+    // navbar.onblur(); // Remove focus from the navbar
+  }
+
 
 }
